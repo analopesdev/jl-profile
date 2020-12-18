b6ce04c767ea4d8fd87903523c4009d243e97ad2
analopesdev <analopesdemorais@outlook.com>
2020-12-18 09:19:03 -0300

    Preloader:  making the preloader screen javascript

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
-var btnContact = document.querySelector('.jl-btn-contact');
+window.addEventListener('load', start)
 
-btnContact.addEventListener('click', function(){
-  var boxContact = document.querySelector('.jl-contact-info')
-  boxContact.classList.toggle('jl-is-open')
-  this.classList.toggle('jl-change-icon')
-})
+function start(){
+  pagePreloader()
+  buttonContact()
+}
+
+// Page Preload
+function pagePreloader(){
+  var pagePreloader = document.querySelector('.jl-preloader')
+
+  this.setTimeout(function(){
+    pagePreloader.classList.add('jl-fade-out')
+  }, 2000)
+}
+
+// Abrindo e fechando contato
+function buttonContact(){
+  var btnContact = document.querySelector('.jl-btn-contact')
+
+  function handlerToggleButton(){
+      var boxContact = document.querySelector('.jl-contact-info')
+      boxContact.classList.toggle('jl-is-open')
+      this.classList.toggle('jl-change-icon')
+    }
+  
+    btnContact.addEventListener('click', handlerToggleButton)
+  }
